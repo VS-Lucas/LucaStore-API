ad45a0023ec10e5e264f21a9d6b28a07fe771da5
VS-Lucas <ufpelucasv@gmail.com>
2024-05-27 22:44:56 -0300

    feat: seed script in package.json

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,16 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreateOrderDto } from './dtos/create-order.dto';
-import { UpdateOrderDto } from './dtos/update-order.dto';
 import { OrderDto } from './dtos/OrderDto';
 import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class OrderService {
   constructor(private prisma: PrismaService) {}
-
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
-  }
 
   async findAllByUserId(userId: number): Promise<OrderDto[]> {
     const orders = await this.prisma.order.findMany({
@@ -51,17 +45,5 @@ export class OrderService {
 
     return orders;
 
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
-  }
-
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} order`;
   }
 }
